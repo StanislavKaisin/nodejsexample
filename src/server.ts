6e97a6a72b26e1a0f4495a3e5cca1636523e0ce9
StanislavKaisin <41913995+StanislavKaisin@users.noreply.github.com>
2021-08-01 15:01:03 +0300

    api rules

--- src/server.ts
+++ src/server.ts
@@ -24,6 +24,18 @@ router.use((req, res, next) => {
 });
 
 /** Parese the request */
-
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
+
+/**Rules of API */
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE POST PUT');
+    return res.status(200).json({});
+  }
+});
